5f11ea1f8e63345b196370aaae0767335650ff39
Avanapu Manohar <manoharavanapu8886@gmail.com>
2024-10-15 15:52:39 +0530

    Initial commit

---
+++ lib/classes data/ClassesData.dart
@@ -0,0 +1,11 @@
+class Class {
+  final String name;
+  final String time;
+  final String location;
+
+  Class({required this.name, required this.time, required this.location});
+}
+List classes= [Class(name: 'CSE (A section)', time: '9:00 AM - 9:50 AM', location: 'Room 503'),
+  Class(name: 'ECE (A Section)', time: '9:50 AM - 10:40 AM', location: 'Room 404'),
+  Class(name: 'AI&DS(CSE)', time: '10:40 AM - 11:30 AM', location: 'Room 410'),
+  Class(name: 'ECE (B Section)', time: '11:30 AM - 12:40 PM', location: 'Room 404'),];
