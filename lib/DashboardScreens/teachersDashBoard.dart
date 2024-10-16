3e564ba7c1fbe6adb2d424c0de2d7ad55b8253c3
Avanapu Manohar <manoharavanapu8886@gmail.com>
2024-10-16 13:09:15 +0530

    Git commits

--- lib/DashboardScreens/teachersDashBoard.dart
+++ lib/DashboardScreens/teachersDashBoard.dart
@@ -1,7 +1,7 @@
-import 'package:attendence_app/DashboardScreens/attendenceReport.dart';
-import 'package:attendence_app/classes%20data/ClassesData.dart';
-import 'package:attendence_app/dashboard/attendenceReport.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../dashboard/attendenceReport.dart';
 
 class TeachersDashboard extends StatelessWidget {
   const TeachersDashboard({super.key});
@@ -14,61 +14,78 @@ class TeachersDashboard extends StatelessWidget {
         backgroundColor: Colors.lightBlue,
         centerTitle: true,
         title: const Text(
-          'List of Classes Taught',
+          'Classes Taught',
           style: TextStyle(
-              color: Color(0xFF081A52),
-              fontWeight: FontWeight.w600,
-              fontSize: 22),
+            color: Color(0xFF081A52),
+            fontWeight: FontWeight.w600,
+            fontSize: 22,
+          ),
         ),
       ),
       body: Container(
         decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                colors: [Colors.lightBlue, Colors.orangeAccent],
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft)),
+          gradient: LinearGradient(
+            colors: [Colors.lightBlue, Colors.orangeAccent],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          ),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
               child: Text(
-                'Select a class to Post attendance ',
+                'Select a Class to Post Attendance',
                 style: TextStyle(
                   color: Color(0xFF081A52),
-                  fontSize: 16,
+                  fontSize: 18,
                   fontWeight: FontWeight.bold,
                 ),
               ),
             ),
             Expanded(
-              child: ListView.builder(
-                itemCount: classes.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Card(
-                    margin: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 5.0),
-                    color: Color(0xFFAEBDD0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.class_,
-                        color: Color(0xFF081A52),
-                      ),
-                      title: Text(
-                        classes[index].name,
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w700,
-                            color: Color(0xFF081A52)),
-                      ),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => AttendenceReport(),
+              child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance.collection('classes').snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  final classes = snapshot.data!.docs;
+                  if (classes.isEmpty) {
+                    return const Center(child: Text('No classes available'));
+                  }
+                  return ListView.builder(
+                    itemCount: classes.length,
+                    itemBuilder: (context, index) {
+                      var classData = classes[index].data() as Map<String, dynamic>;
+                      return Card(
+                        margin: const EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 5.0),
+                        color: const Color(0xFFAEBDD0),
+                        child: ListTile(
+                          leading: const Icon(
+                            Icons.class_,
+                            color: Color(0xFF081A52),
                           ),
-                        );
-                      },
-                    ),
+                          title: Text(
+                            classData['name'] ?? 'Class Name',
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                              color: Color(0xFF081A52),
+                            ),
+                          ),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => AttendenceReport(),
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    },
                   );
                 },
               ),
